11768159cdb19136f1346e5c7d80fb270ad75cd5
Nimith Gangadhar Shetty <Nimith.Shetty@asl.mum.sst>
2024-09-14 21:00:56 +0530

    initial commit

---
+++ CookieDemo/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CookieDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            HttpCookie cookie = Request.Cookies["VisitCount"];
+            if (cookie != null)
+            {
+                int count = int.Parse(cookie.Value) + 1;
+                cookie.Value = count.ToString();
+                Response.Cookies.Add(cookie);
+                ViewBag.Count = count;
+                return View();
+            }
+            else
+            {
+                cookie = new HttpCookie("VisitCount", "1");
+                cookie.Expires = DateTime.Now.AddDays(7);
+                Response.Cookies.Add(cookie);
+                return View();
+            }
+        }
+
+        public ActionResult DisplayPage()
+        {
+            return View();
+        }
+    }
+}
